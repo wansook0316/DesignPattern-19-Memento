2a15f0840c99faca65361bc3f77b338c49e06baf
wansook0316 <wansook0316@gmail.com>
2023-02-07 13:15:55 +0900

    update: main

--- Memento/main.swift
+++ Memento/main.swift
@@ -7,5 +7,41 @@
 
 import Foundation
 
-print("Hello, World!")
+internal func main() {
 
+    let start = Coordinate(x: 0, y: 0)
+    let destination = Coordinate(x: 10, y: 10)
+
+    let walker = Walker(current: start, target: destination)
+    let actions = ["UP", "RIGHT", "DOWN", "LEFT"]
+
+    var minDistance = Double.greatestFiniteMagnitude
+    var memento: Walker.Memento?
+
+    while true {
+        let action = actions[Int.random(in: 0..<actions.count)]
+
+        walker.walk(with: action)
+
+        let remainDistance = walker.remainingDistance
+        print(remainDistance)
+
+        if remainDistance == .zero {
+            break
+        }
+
+        // 거리가 작아지면 저장함
+        if minDistance > remainDistance {
+            minDistance = remainDistance
+            memento = walker.createMemento()
+        // 거리가 커지는 경우 이전 상태를 불러옴
+        } else if let memento {
+            walker.restore(with: memento)
+        }
+    }
+
+    print("walker's path: \(walker.description)")
+
+}
+
+main()
